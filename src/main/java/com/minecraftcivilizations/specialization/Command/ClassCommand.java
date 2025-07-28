55caf9f61de0e01c3d69d21d209cf0a4b6245483
bananababoo <banababnana@gmail.com>
2025-07-28 12:04:05 -0500

    change plugins to new format

--- src/main/java/com/minecraftcivilizations/specialization/Command/ClassCommand.java
+++ src/main/java/com/minecraftcivilizations/specialization/Command/ClassCommand.java
@@ -1,28 +1,24 @@
 package com.minecraftcivilizations.specialization.Command;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
 import com.minecraftcivilizations.specialization.GUI.MainMenuGUI;
 import com.minecraftcivilizations.specialization.Specialization;
-import minecraftcivilizations.com.minecraftCivilizationsCore.GUI.ListGUI;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+@CommandAlias("class")
+public class ClassCommand extends BaseCommand {
 
-public class ClassCommandExecutor implements CommandExecutor {
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+    @Default
+    public void onClass(@NotNull CommandSender sender) {
         if (sender instanceof Player player && player.isOp()) {
             Specialization.logger.info("Opening GUI for " + player.getName());
             new MainMenuGUI("Your Specialization Stats").open(player);
             Specialization.logger.info("Opened GUI for " + player.getName());
         }
-        return true;
     }
+
 }
---
+++ src/main/java/com/minecraftcivilizations/specialization/Command/TownsCommand.java
@@ -0,0 +1,32 @@
+package com.minecraftcivilizations.specialization.Command;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import com.minecraftcivilizations.specialization.Distance.Town;
+import com.minecraftcivilizations.specialization.Distance.TownManager;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+@CommandAlias("towns")
+public class TownsCommand extends BaseCommand {
+
+    @Default
+    @CommandPermission("towndetector.list")
+    public void onTowns(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(ChatColor.GREEN + "=== Detected Towns ===");
+        if (TownManager.getTowns().isEmpty()) {
+            sender.sendMessage(ChatColor.YELLOW + "No towns detected yet.");
+        } else {
+            for (int i = 0; i < TownManager.getTowns().size(); i++) {
+                Town town = TownManager.getTowns().get(i);
+                sender.sendMessage(ChatColor.AQUA + "Town " + (i + 1) + ": " +
+                        ChatColor.WHITE + town.getBedCount() + " beds at " +
+                        TownManager.formatLocation(town.getCenterLocation()));
+            }
+        }
+    }
+
+}
