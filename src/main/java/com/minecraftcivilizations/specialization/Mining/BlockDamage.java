1922b107cd1a6787fae45015a64f45590680ad07
DNFneca <bojovicnemanja2@gmail.com>
2025-07-30 19:51:33 +0200

    Refactor config type usage and update core dependency
    Updated the MinecraftCivilizationsCore dependency to 0.5.0 and refactored usages of TypeToken in configuration lookups to use the diamond operator for improved type inference. Simplified lore setting in SetLoreCommand and adjusted recipe registration in Recipes.java to use CustomItemRegistry. Added 'Weight' as a plugin dependency in plugin.yml.

--- src/main/java/com/minecraftcivilizations/specialization/Mining/BlockDamage.java
+++ src/main/java/com/minecraftcivilizations/specialization/Mining/BlockDamage.java
@@ -123,7 +123,7 @@ public class BlockDamage {
         }
         double damage;
 
-        damage = speedMultiplier / (Double) SpecializationConfig.getBlockHardnessConfig().get(block.getType(), new TypeToken<Double>() {}.getType());
+        damage = speedMultiplier / SpecializationConfig.getBlockHardnessConfig().get(block.getType(), Double.class);
 
         damage /= 30;
 
@@ -137,7 +137,7 @@ public class BlockDamage {
 
     public void playerBreakBlock(Player player, Block block) {
         CustomPlayer customPlayer = (CustomPlayer) MinecraftCivilizationsCore.getInstance().getCustomPlayerManager().getCustomPlayer(player.getUniqueId());
-        Pair<SkillType, Double> pair = SpecializationConfig.getXpGainFromBreakingConfig().get(block.getType(), new TypeToken<Pair<SkillType, Double>>() {}.getType());
+        Pair<SkillType, Double> pair = SpecializationConfig.getXpGainFromBreakingConfig().get(block.getType(), new TypeToken<>() {});
         Specialization.logger.info(block.getType().name() + " ");
         customPlayer.addSkillXp(pair.firstValue(), pair.secondValue());
 
--- src/main/java/com/minecraftcivilizations/specialization/Recipe/Recipes.java
+++ src/main/java/com/minecraftcivilizations/specialization/Recipe/Recipes.java
@@ -2,6 +2,7 @@ package com.minecraftcivilizations.specialization.Recipe;
 
 import com.minecraftcivilizations.specialization.Specialization;
 import minecraftcivilizations.com.minecraftCivilizationsCore.Item.CustomItem;
+import minecraftcivilizations.com.minecraftCivilizationsCore.Item.CustomItemRegistry;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
@@ -15,34 +16,15 @@ import java.util.List;
 public class Recipes {
 
     public static void init() {
-        ItemStack itemStack = CustomItem.newCustomItem(Material.PAPER,
-            Component.text("Bandage"),
-            List.of(Component.empty(),
-                    Component.text("If used by a healer able to heal players")
-            ),
-            Specialization.getInstance());
-
+        ItemStack itemStack = ItemStack.of(Material.PAPER);
         ShapelessRecipe shapelessRecipe = new ShapelessRecipe(new NamespacedKey(Specialization.getInstance(), "bandage"), itemStack);
         shapelessRecipe.addIngredient(new ItemStack(Material.PAPER, 8));
         shapelessRecipe.addIngredient(new ItemStack(Material.SUGAR_CANE));
 
-        Bukkit.addRecipe(shapelessRecipe, true);
+        CustomItem newCustomItem = new CustomItem();
+        newCustomItem.setItem(itemStack);
 
-        itemStack = CustomItem.newCustomItem(Material.PAPER,
-                Component.text("Iron Chestplate Blueprint").color(NamedTextColor.WHITE),
-                List.of(
-                        Component.empty(),
-                        Component.text("Used for crafting of").color(NamedTextColor.WHITE),
-                        Component.text("the Iron Chestplate").color(NamedTextColor.WHITE),
-                        Component.empty(),
-                        Component.text("Crafted by the most").color(NamedTextColor.GRAY),
-                        Component.text("esteemed of librarians").color(NamedTextColor.GRAY)
-                ),
-                Specialization.getInstance());
-
-        shapelessRecipe = new ShapelessRecipe(new NamespacedKey(Specialization.getInstance(), "iron_chestplate_blueprint"), itemStack);
-        shapelessRecipe.addIngredient(new ItemStack(Material.PAPER, 8));
-        shapelessRecipe.addIngredient(new ItemStack(Material.IRON_INGOT));
+        CustomItemRegistry.register(new NamespacedKey(Specialization.getInstance(), "bandage"), newCustomItem);
 
         Bukkit.addRecipe(shapelessRecipe, true);
     }
