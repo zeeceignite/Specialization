661eb654b18591fa6193a5a9f7bfba0da6d00719
DNFneca <bojovicnemanja2@gmail.com>
2025-07-31 16:58:16 +0200

    Use the code as an example, the given code makes a bandage, that works kinda no checks no nothing!
    Added display name, lore, and ability to the bandage item in Recipes.java. Registered a new ability for the bandage and improved its initialization. Removed the unused Pair.java file and fixed a type cast in CustomPlayer. Set lore color to white in SetLoreCommand.

--- src/main/java/com/minecraftcivilizations/specialization/Recipe/Recipes.java
+++ src/main/java/com/minecraftcivilizations/specialization/Recipe/Recipes.java
@@ -1,8 +1,15 @@
 package com.minecraftcivilizations.specialization.Recipe;
 
+import com.minecraftcivilizations.specialization.Config.SpecializationConfig;
+import com.minecraftcivilizations.specialization.Player.CustomPlayer;
+import com.minecraftcivilizations.specialization.Skill.SkillType;
 import com.minecraftcivilizations.specialization.Specialization;
+import minecraftcivilizations.com.minecraftCivilizationsCore.Ability.AbilityCastEvent;
+import minecraftcivilizations.com.minecraftCivilizationsCore.Ability.CustomAbility;
+import minecraftcivilizations.com.minecraftCivilizationsCore.Ability.CustomItemAbilityRegistry;
 import minecraftcivilizations.com.minecraftCivilizationsCore.Item.CustomItem;
 import minecraftcivilizations.com.minecraftCivilizationsCore.Item.CustomItemRegistry;
+import minecraftcivilizations.com.minecraftCivilizationsCore.MinecraftCivilizationsCore;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
@@ -10,6 +17,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
@@ -17,15 +25,35 @@ public class Recipes {
 
     public static void init() {
         ItemStack itemStack = ItemStack.of(Material.PAPER);
-        ShapelessRecipe shapelessRecipe = new ShapelessRecipe(new NamespacedKey(Specialization.getInstance(), "bandage"), itemStack);
-        shapelessRecipe.addIngredient(new ItemStack(Material.PAPER, 8));
-        shapelessRecipe.addIngredient(new ItemStack(Material.SUGAR_CANE));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(Component.text("Bandage").color(NamedTextColor.WHITE));
+        itemStack.setItemMeta(itemMeta);
+
+        CustomAbility customAbility = new CustomAbility();
+        customAbility.setAbilityFunction(customAbilityFunction -> {
+            CustomPlayer customPlayer = (CustomPlayer) MinecraftCivilizationsCore.getInstance().getCustomPlayerManager().getCustomPlayer(customAbilityFunction.getUniqueId());
+            customPlayer.addSkillXp(SkillType.HEALER, 100);
+            Specialization.logger.info("TESTING");
+        });
+        customAbility.setCooldown(10);
+        customAbility.setCastEvent(AbilityCastEvent.RIGHT_CLICK);
+        customAbility.setName("Bandage");
+        customAbility.setDescription("Bandage");
+
+        CustomItemAbilityRegistry.register(new NamespacedKey(Specialization.getInstance(), "bandage"), customAbility);
 
         CustomItem newCustomItem = new CustomItem();
         newCustomItem.setItem(itemStack);
+        newCustomItem.addLore(List.of(Component.empty(),
+                Component.text("Can be used to heal yourself or others.").color(NamedTextColor.WHITE)));
+        newCustomItem.addAbility(new NamespacedKey(Specialization.getInstance(), "bandage"));
 
         CustomItemRegistry.register(new NamespacedKey(Specialization.getInstance(), "bandage"), newCustomItem);
 
+
+        ShapelessRecipe shapelessRecipe = new ShapelessRecipe(new NamespacedKey(Specialization.getInstance(), "bandage"), itemStack);
+        shapelessRecipe.addIngredient(new ItemStack(Material.PAPER, 8));
+        shapelessRecipe.addIngredient(new ItemStack(Material.SUGAR_CANE));
         Bukkit.addRecipe(shapelessRecipe, true);
     }
 }
