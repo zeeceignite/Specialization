0112d8db793020c7e80fde4d5dea07c290e8a877
Shotrush <flipyfun503@gmail.com>
2025-08-06 14:08:57 -0400

    Removed the packet based mining speed and instead used attribute based mining speed.

--- src/main/java/com/minecraftcivilizations/specialization/Mining/BreakBlockListener.java
+++ src/main/java/com/minecraftcivilizations/specialization/Mining/BreakBlockListener.java
@@ -1,18 +1,82 @@
 package com.minecraftcivilizations.specialization.Mining;
 
+import com.google.gson.reflect.TypeToken;
+import com.minecraftcivilizations.specialization.Config.SpecializationConfig;
+import com.minecraftcivilizations.specialization.Player.CustomPlayer;
+import com.minecraftcivilizations.specialization.Skill.SkillType;
+import com.minecraftcivilizations.specialization.util.CoreUtil;
+import minecraftcivilizations.com.minecraftCivilizationsCore.Options.Pair;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.inventory.ItemStack;
+
+import static com.minecraftcivilizations.specialization.Reinforcement.ReinforcementManager.*;
 
 public class BreakBlockListener implements Listener {
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-
         AttributeInstance breakSpeedAttr = event.getPlayer().getAttribute(Attribute.BLOCK_BREAK_SPEED);
         if (breakSpeedAttr != null) {
             breakSpeedAttr.setBaseValue(1.0);
+            Pair<SkillType, Double> pair = SpecializationConfig.getXpGainFromPlacingConfig().get(event.getBlock().getType(), new TypeToken<>() {});
+            CustomPlayer player = CoreUtil.getPlayer(event.getPlayer());
+
+            if(isReinforced(event.getBlock())) {
+                Location dropLocation = event.getBlock().getLocation().add(0.5, 0.5, 0.5);
+
+                if(isHeavilyReinforced(event.getBlock())) {
+                    event.getBlock().getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_INGOT, 1));
+                    event.getPlayer().sendMessage("You have received 1 iron ingot for breaking heavily reinforced blocks!");
+                }
+                if(isLightlyReinforced(event.getBlock())) {
+                    event.getBlock().getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_NUGGET, 1));
+                    event.getPlayer().sendMessage("You have received 1 iron nugget for breaking lightly reinforced blocks!");
+                }
+                removeReinforcement(event.getBlock());
+            }
+            player.addSkillXp(pair.firstValue(), pair.secondValue());
         }
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        event.blockList().forEach(block -> {
+            if(isReinforced(block)) {
+                Location dropLocation = block.getLocation().add(0.5, 0.5, 0.5);
+
+                if(isHeavilyReinforced(block)) {
+                    block.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_INGOT, 1));
+                }
+                if(isLightlyReinforced(block)) {
+                    block.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_NUGGET, 1));
+                }
+                removeReinforcement(block);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onBlockExplode(BlockExplodeEvent event) {
+        event.blockList().forEach(block -> {
+            if(isReinforced(block)) {
+                Location dropLocation = block.getLocation().add(0.5, 0.5, 0.5);
+
+                if(isHeavilyReinforced(block)) {
+                    block.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_INGOT, 1));
+                }
+                if(isLightlyReinforced(block)) {
+                    block.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.IRON_NUGGET, 1));
+                }
+                removeReinforcement(block);
+            }
+        });
     }
 }
