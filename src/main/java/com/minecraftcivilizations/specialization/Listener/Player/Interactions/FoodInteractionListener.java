31c97171fc442eb25c0997a5c74305eced9d2f6c
bananababoo <banababnana@gmail.com>
2025-08-16 16:02:48 -0500

    golden apple buff

--- src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/FoodInteractionListener.java
+++ src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/FoodInteractionListener.java
@@ -89,12 +89,23 @@ public class FoodInteractionListener implements Listener {
             giveKelpEffects(player);
         }
 
+        if(item.getType().equals(Material.GOLDEN_APPLE)) {
+            giveGoldenAppleEffects(player);
+        }
+
     }
 
     private void giveKelpEffects(Player player){
         if(new Random().nextDouble() < .2) {
             player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 20 * 10, 1));
             player.sendRichMessage("<#456e55>You feel a little seasick from eating the kelp.");
+        }
+    }
+
+    private void giveGoldenAppleEffects(Player player){
+        if(new Random().nextDouble() < .4) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, 20 * 60, 2));
+            player.sendRichMessage("<#dbae32>You feel solidified by the golden nature of the apple.");
         }
     }
 
