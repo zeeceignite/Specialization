cbdadcf63d2190628013b999ef28350aacab4365
bananababoo <banababnana@gmail.com>
2025-10-05 18:28:33 -0500

    add null check to fishing

--- src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/FishingListener.java
+++ src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/FishingListener.java
@@ -1,7 +1,6 @@
 package com.minecraftcivilizations.specialization.Listener.Player.Interactions;
 
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
@@ -10,7 +9,7 @@ public class FishingListener implements Listener {
 
     @EventHandler
     public void onFish(PlayerFishEvent event) {
-        if(event.getCaught() instanceof Item item) {
+        if(event.getCaught() != null && event.getCaught() instanceof Item item) {
             if(item.getItemStack().getType().toString().toLowerCase().matches("(?i)(Enchanted Book|Bow)")){
                 if(Math.random() > 0.0005){
                     event.setCancelled(true);
