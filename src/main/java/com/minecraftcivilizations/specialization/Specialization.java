8e9a46f36c028c62711690db6dc21ca3d3c85c2a
Shotrush <flipyfun503@gmail.com>
2025-10-17 21:53:50 -0400

    Bandage fix? Hotfix #1C?

--- src/main/java/com/minecraftcivilizations/specialization/Specialization.java
+++ src/main/java/com/minecraftcivilizations/specialization/Specialization.java
@@ -146,7 +146,7 @@ public final class Specialization extends JavaPlugin {
         Blueprints.init();
         XpGainMonitor.init();
 
-        Bukkit.updateRecipes();
+        //Bukkit.updateRecipes() clears custom recipes added via Bukkit.addRecipe() and it automatically syncs to client anyway when they join, so I removed it.
 
 
         MinecraftCivilizationsCore.getInstance().getCustomPlayerManager().setCustomPlayerClass(CustomPlayer.class);
