ba4097eef87d46e48569e9d32fbf1a4ae7460715
Joshua Crane <Joshua.crane.me@gmail.com>
2025-11-12 23:29:24 -0500

    ouch

--- src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/RightClickListener.java
+++ src/main/java/com/minecraftcivilizations/specialization/Listener/Player/Interactions/RightClickListener.java
@@ -37,7 +37,7 @@ public class RightClickListener implements Listener {
         if(event.getClickedBlock().getType().equals(Material.SWEET_BERRY_BUSH) && cPlayer.getSkillLevel(SkillType.FARMER) < 2){
             if(Math.random() < 0.2){
                 player.damage(1);
-                player.sendRichMessage("<#d16060>Ouch! You pricked your finger on the berry bush.");
+                player.chat("Ouch!");
             }
         }
 
